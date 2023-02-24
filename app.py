79ead197acb352f0a99a7df3f97eccfba915a708
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-02-24 17:05:46 +0100

    jsonify

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask
-from flask_restful import Api, Resource
 import sys
+
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorldResource(Resource):
     def get(self):
-        return {
-            "message": "Hello from the REST API!"
-            }
+        result = {"data": "There are no events for today!"}
+        return jsonify(result)
 
-api.add_resource(HelloWorldResource, '/hello')
 
+api.add_resource(HelloWorldResource, '/event/today')
 
 # do not change the way you run the program
 if __name__ == '__main__':
