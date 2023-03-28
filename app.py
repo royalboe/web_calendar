8953a52af403b341b36485a836d87c6deec538ad
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2023-03-28 21:38:12 +0100

    Task 3 completed

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 import sys
-from flask import Flask, jsonify
+from flask import Flask
 from flask_restful import Api, Resource, reqparse, inputs
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
 import datetime
 
-
 app = Flask(__name__)
-db = SQLAlchemy()
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event.db'
+db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///name.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret_key'
 
 # initialize the app with the extension
-db.init_app(app)
-
-
+# db.init_app(app)
 
 parser = reqparse.RequestParser()
 
@@ -47,53 +44,57 @@ class Event(db.Model):
 
     def __str__(self):
         return f"{self.date} {self.event}"
-    
+
+
 with app.app_context():
     db.create_all()
 
+
+class EventSchema(Schema):
+    id = fields.Integer()
+    event = fields.String()
+    date = fields.DateTime(dt_format='iso8601')
+
+
 class TodayEvent(Resource):
     def get(self):
+        schema = EventSchema(many=True)
         events = Event.query.filter(Event.date == datetime.date.today()).all()
         if events:
-            result = {"data": []}
-            for event in events:
-                result["data"].append({"id": event.id, "event": event.event, "date": datetime.datetime.strftime(event.date,"%Y-%m-%d")})
-            return jsonify(result)
-        
+            return schema.dump(events)
 
         result = {"data": "There are no events for today!"}
-        return jsonify(result)
+        return schema.dump(result)
 
 
 class PostEvent(Resource):
     def post(self):
+        # schema = EventSchema()
         args = parser.parse_args()
         result = {
             "message": "The event has been added!",
             "event": args['event'],
-            "date": datetime.datetime.strftime(args['date'],"%Y-%m-%d")
+            "date": datetime.datetime.strftime(args['date'], "%Y-%m-%d")
         }
         db.session.add(Event(date=args['date'].date(), event=args['event']))
         db.session.commit()
-        return jsonify(result)
+        # return schema.dump(args)
+        return result, 200
 
 
 class GetAllEvent(Resource):
     def get(self):
+        schema = EventSchema(many=True)
         events = Event.query.all()
         if events:
-            result = {"data": []}
-            for event in events:
-                result["data"].append({"id": event.id, "event": event.event, "date": datetime.datetime.strftime(event.date,"%Y-%m-%d")})
-            return jsonify(result)
+            return schema.dump(events)
         result = {"data": "There are no events!"}
-        return jsonify(result)
+        return schema.dump(result)
 
 
 api.add_resource(TodayEvent, '/event/today')
 api.add_resource(PostEvent, '/event')
 api.add_resource(GetAllEvent, '/event')
-
 
 # do not change the way you run the program
 if __name__ == '__main__':
@@ -101,4 +102,4 @@ if __name__ == '__main__':
         arg_host, arg_port = sys.argv[1].split(':')
         app.run(host=arg_host, port=arg_port)
     else:
-        app.run(debug=True)
+        app.run()
